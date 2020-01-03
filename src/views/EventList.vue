1ef7feabfc4ff2c3f3ef150798a6aafc9f0ce1e0
gameorstudy <xiongjianyu1998@163.com>
2020-01-04 06:15:03 +0800

    submit files

---
+++ src/views/EventList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '../services/EventService.js'
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents() // Does a get request
+      .then(response => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch(error => {
+        console.log('There was an error: ', error.response) // Logs out the error
+      })
+  }
+}
+</script>
